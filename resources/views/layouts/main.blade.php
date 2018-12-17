1ed51e3cf9f891d3c6b57acc95fa89ee863f63fb
francodicampli <francodicampli@gmail.com>
2018-12-17 17:29:20 -0300

    Adminlte, vue y auth funcionando

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -9,14 +9,14 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
-
+  <meta name="csrf-token" content="{{ csrf_token() }}">
   <title>AdminLTE 3 | Starter</title>
 
   <link rel="stylesheet" href="/css/app.css">
  
 </head>
-<body class="hold-transition sidebar-mini">
-<div class="wrapper">
+<body class="hold-transition sidebar-mini" >
+<div class="wrapper" id="app">
 
   <!-- Navbar -->
   <nav class="main-header navbar navbar-expand bg-white navbar-light border-bottom">
@@ -143,7 +143,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <div class="content">
       <div class="container-fluid">
        
-        <!-- /.row -->
+        @yield('content')
       </div><!-- /.container-fluid -->
     </div>
     <!-- /.content -->
